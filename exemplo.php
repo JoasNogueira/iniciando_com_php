9cf32f721109324f9d52cdfb12455c23c3ac7cb6
Joas Nogueira <joassantos30@hotmail.com>
2019-05-03 21:13:57 -0300

    Formulario para imprimir Nome e Sobrenome

--- exemplo.php
+++ exemplo.php
@@ -1,6 +1,20 @@
 <?php 
 
-echo "Hello World";
+function mostrarNome(){
+
+	$nome = "";
+	$sobrenome = "";
+	if (isset($_GET["nome"])&&isset($_GET["sobrenome"])&&!empty ( $_GET["nome"] )&&!empty($_GET["sobrenome"])) {
+		$nome = $_GET["nome"];
+		$sobrenome = $_GET["sobrenome"];
+	}
+	else{
+		$nome = "Nome ou sobrenome não podem estar vazios";
+	}
+	echo $nome.' '.$sobrenome;
+}
+mostrarNome();
+
 
 
 ?>
